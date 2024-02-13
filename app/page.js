7ed2d69611f2c95ff60fb72daeacbfd6ce35a61d
JRomuel <johnromuelmendoza@yahoo.com>
2024-02-13 18:56:28 +0800

    fist commit

--- app/page.js
+++ app/page.js
@@ -1,94 +1,59 @@
+'use client'
+import { useRef, useState } from "react";
 import Image from "next/image";
 import styles from "./page.module.css";
+import { motion, animate  } from "framer-motion"
 
 export default function Home() {
+
+  const texts = ['No', 'Are you sure?', 'No Please :(', "Meow :(", "Don't Do this to me :(", "Youre Breaking my Heart", "I', gonna cry..." ];
+  const images = ['/cat-giving-flower.jpg', '/crycat-crying-cat.gif', '/cat.gif', "/mean-cat.jpg", "/crazy-cat.jpg", "/sad-cat.jpg", "/crying-cat-2.jpg" ];
+  
+  const [buttonText, setButtonText] = useState('No');
+  const [mainImage, setMainImage] = useState('/cat-giving-flower.jpg');
+  let [count, setCount] = useState(0);
+  const button = useRef(null);
+
+  const onMouseEnter = () => {
+    
+    if(count >= texts.length - 1){
+      setCount(0);
+    }else{
+      setCount(++count);
+    }
+    setButtonText(texts[count]);
+    setMainImage(images[count]);
+    animate(button.current, {top: Math.floor(Math.random() * (window.innerHeight - 100)), left: Math.floor(Math.random() * (window.innerWidth - 100))});
+
+  };
+  const sheSaidYes = () => {
+    
+    setMainImage('/in-loved-cat.jpg');
+
+  };
+
+  
+
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
 
       <div className={styles.center}>
         <Image
+          style={{objectFit:'cover'}}
           className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
+          src={mainImage}
+          alt="Cat Flower"
+          width={300}
+          height={300}
           priority
         />
+
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <h1 className={styles.title}>Will you be my Valentine?</h1>
+      <div className={styles.buttons}>
+          <button type="button" onClick={sheSaidYes} className={styles.yes}>Yes</button>
+          <motion.button ref={button} type="button" onClick={onMouseEnter} onMouseEnter={onMouseEnter} className={styles.no}>{buttonText}</motion.button>
       </div>
     </main>
   );
